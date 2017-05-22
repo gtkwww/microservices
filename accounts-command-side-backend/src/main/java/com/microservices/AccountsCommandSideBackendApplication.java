2e943ecd0313e64dc962888b94ba8b323ad494f4
gtkwww <txwz2006@126.com>
2017-05-22 21:16:46 +0800

    初始化项目

---
+++ accounts-command-side-backend/src/main/java/com/microservices/AccountsCommandSideBackendApplication.java
@@ -0,0 +1,12 @@
+package com.microservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountsCommandSideBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountsCommandSideBackendApplication.class, args);
+	}
+}
---
+++ accounts-command-side-service/src/main/java/com/microservices/AccountsCommandSideServiceApplication.java
@@ -0,0 +1,12 @@
+package com.microservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountsCommandSideServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountsCommandSideServiceApplication.class, args);
+	}
+}
